20c0060b65202fcc3de0e9c8e2cea2676b681155
dodma1256 <dodam1256@naver.com>
2020-07-20 07:09:45 +0900

    network byte order

--- nbo.cpp
+++ nbo.cpp
@@ -2,24 +2,28 @@
 # include <stdint.h>
 # include <netinet/in.h>
 
+uint32_t my_ntohs2(uint32_t n){
+	return (n&0xff000000) >> 24 | (n&0x00ff0000) >> 8
+		| (n&0x0000ff00) << 8 | (n&0x000000ff) << 24;}
+     
+
 int main(int argc, char*argv[]){
 
         if (argc != 3){
                 printf("usage : %s <file1> <file2>\n", argv[0]);
-        return 0;
-	}
+        return 0;}
 
-        FILE *file1, *file2 ;
-        uint32_t file1num, file2num,sum;
+        FILE *file1, *file2;
+	uint32_t file1num , file2num , sum;
 
         file1 = fopen(argv[1], "r");
         file2 = fopen(argv[2], "r");
 
-        fread(&file1num, 1 , 5 , file1);
-        fread(&file2num, 1 , 5 , file2);
+        fread(&file1num, 1 , 5  , file1);
+        fread(&file2num, 1 , 5  , file2);
 
-        file1num = ntohs(file1num);
-        file2num = ntohs(file2num);
+	file1num = my_ntohs2(file1num);
+        file2num = my_ntohs2(file2num);
 
         sum = file1num + file2num ;
 
@@ -28,5 +32,4 @@ int main(int argc, char*argv[]){
         fclose(file1);
         fclose(file2);
         }
-
 
